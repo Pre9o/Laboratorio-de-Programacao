037ed84f399cff2a68f5bd513eaed51dcf51c60b
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2022-09-22 14:49:02 -0300

    Meter per second to kilometers per hour
    Program to convert meters per second to kilometers per hour

---
+++ meters_to_kilometers.c
@@ -0,0 +1,19 @@
+/*Rafael Carneiro Pregardier*/
+
+/*Program made to transfer the speed from meters per second to kilometers per hour.*/
+
+#include <stdio.h> 
+
+int main(){
+    int m; 
+    float k;
+
+    printf("Enter the speed in meters per second:\n");
+
+    scanf("%d", &m);
+    k=m*3.6;
+
+    printf("The speed in kilometers per hour is: %.2f km/h", k);
+
+    return 0;
+}

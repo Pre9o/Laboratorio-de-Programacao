45db2eefd00fc4b3476584f292a8e2d1038f38cb
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2022-09-22 15:12:54 -0300

    Area of a triangle
    Program made to calculate a area of a triangle

---
+++ area_of_triangle.c
@@ -0,0 +1,23 @@
+/*Rafael Carneiro Pregardier*/
+
+/*Program maded to express the area of ​​a triangle*/
+
+
+#include <stdio.h>
+
+int main(){
+    int b, h;
+    float a;
+
+    printf("Enter the value of the triangle base:\n");
+    scanf("%d", &b);
+
+    printf("Now enter the value of the height of the triangle:\n");
+    scanf("%d", &h);
+
+    a=(float)(b*h)/2;
+
+    printf("The area of triangle is: %.2f\n", a);
+
+    return 0;
+}
